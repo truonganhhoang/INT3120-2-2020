a03aaee975d5cd85bd693efeac3c45d4fe30bde7
meepawn2019 <meepawn2018@gmail.com>
2020-09-21 12:17:27 +0700

    testing

--- 19/vietjack_mobile_app/lib/common/Header.dart
+++ 19/vietjack_mobile_app/lib/common/Header.dart
@@ -1,17 +1,46 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class Header extends StatefulWidget {
+  bool _isLoaded;
+  double width;
+  double height;
+  Header({Key key, this.width, this.height}) : super(key: key);
   @override
-  _Header
-State createState() => _Header
-State();
+  _HeaderState createState() => _HeaderState();
 }
 
 class _HeaderState extends State<Header> {
+  double left, right = 0;
   @override
   Widget build(BuildContext context) {
+    Future.delayed(Duration(milliseconds: 1), () {
+      setState(() {
+        left = widget.width * 0.5;
+        right = widget.width * 0.5;
+      });
+    });
+
     return Container(
-      child: new Row()
+      child: new Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          new AnimatedContainer(
+            duration: Duration(seconds: 2),
+            color: Colors.red,
+            width: left,
+            height: 200,
+          ),
+          new AnimatedContainer(
+            duration: Duration(seconds: 2),
+            color: Colors.blue,
+            width: right,
+            height: 200,
+          )
+        ],
+      ),
     );
   }
 }
