0d88c13aaa2c29dcbaa1eb56d5bef72eee247999
lawlietsnow <thotang19@gmail.com>
2020-09-23 23:21:53 +0700

    animationController

--- 19/vietjack_mobile_app/lib/Header.dart
+++ 19/vietjack_mobile_app/lib/Header.dart
@@ -1,43 +1,69 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Header extends StatefulWidget {
   double width;
   double height;
+
   Header({Key key, this.width, this.height}) : super(key: key);
 
   @override
   _HeaderState createState() => _HeaderState();
 }
 
-class _HeaderState extends State<Header> {
+class _HeaderState extends State<Header> with SingleTickerProviderStateMixin{
   double left = 0, right = 0;
+  double myOpacity = 0;
+  AnimationController animationController;
   @override
-  Widget build(BuildContext context) {
-    Future.delayed(Duration(milliseconds: 1), () {
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    left = widget.width/2;
+    animationController = new AnimationController(vsync: this,duration: Duration(seconds: 1));
+    animationController.forward();
+    animationController.addListener(() {
       setState(() {
-        left = widget.width * 0.5;
-        right = widget.width * 0.5;
+        //left=widget.width * 0.5*animationController.value;
+        myOpacity = animationController.value ;
+        right = widget.width * 0.5 * animationController.value;
+
       });
     });
-    return Container(
-      child: Row(
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children:
-            <Widget>[
-                new AnimatedContainer(
-                  duration: Duration(seconds: 2),
-                  color: Colors.red,
-                  width: left,
-                  height: 200,
+        children: <Widget>[
+          new Opacity(
+            opacity: myOpacity,
+            child: new Container(
+              width: left,
+              height: 200,
+              child: Center(
+                child: new Text(
+                  "Kiên Đẹp Trai",
+                  style: new TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'GoodDog',
+                    fontSize: 30,
+                    color: Colors.pink[200]
+                  ),
                 ),
-                new AnimatedContainer(
-                  duration: Duration(seconds: 2),
-                  color: Colors.blue,
-                  width: right,
-                  height: 200,
-                ),
-              ]
-      ),
-    );
+              )
+            ),
+          ),
+          new Container(
+            color: Colors.blue,
+            width: right,
+            height: 200,
+            child: new Image(
+              image: AssetImage('assets/idea.jpg'),
+              fit: BoxFit.fitHeight
+            ),
+          ),
+        ]);
   }
 }
