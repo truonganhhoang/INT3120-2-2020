6458d9ce99ee9092a3370771e218085426bb074a
daipthraomng <52918894+daipthraomng@users.noreply.github.com>
2020-10-07 12:27:27 +0700

    #8 ADD tạo model class cho các câu hỏi trắc nghiệm, thêm các hình ảnh dữ liệu cho câu hỏi mẫu như trong thiết kế lesson1 figma

--- 28/duolingo_demo/lib/main.dart
+++ 28/duolingo_demo/lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+//import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
+//import 'package:firebase_core/firebase_core.dart';
 import './screens/home.dart';
 
 void main() => runApp(MyApp());
