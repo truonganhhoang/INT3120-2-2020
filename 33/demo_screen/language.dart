891437fd6a3ae89251f288489fba61ed035dc2c1
Minh Trần Quang <minhtq30102000@gmail.com>
2020-10-09 01:56:31 +0700

    language

--- 33/demo_screen/language.dart
+++ 33/demo_screen/language.dart
@@ -11,9 +11,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
-
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
@@ -31,59 +29,110 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-          backgroundColor: Colors.teal,
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Select Language",style: TextStyle(color: Colors.white,fontSize: 25,fontWeight: FontWeight.bold)),
-                SizedBox(height: 50),
-                Card(
-                  color: Colors.white.withOpacity(0.5),
-                  elevation: 5.0,
-                  child: Container(
-                    width: 300,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        SizedBox(
-                            width: 50,
-                            height: 50,
-                            child: Image.network(
-                                'https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Flag_of_Vietnam.svg/1200px-Flag_of_Vietnam.svg.png')),
-                        SizedBox(width: 20),
-                        Text("VIET NAM",style: TextStyle(color: Colors.white,fontSize: 15,fontWeight: FontWeight.bold)),
-                      ],
-                    ),
+      backgroundColor: Color(0xff006265),
+      body: Center(
+        child: Column(
+
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(height: 100),
+            Text("Select Your Language",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold)),
+            SizedBox(height: 120),
+            Card(
+              elevation: 5.0,
+              color: Color(0xff5CA4A7),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(30),
+                      topLeft: Radius.circular(30),
+                      bottomRight: Radius.circular(30),
+                      topRight: Radius.circular(30)
                   ),
-                ),
-                Card(
-                  elevation: 5.0,
-                  color: Colors.white.withOpacity(0.5),
-                  child: Container(
-                    width: 300,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        SizedBox(
-                            width: 50,
-                            height: 50,
-                            child: Image.network(
-                                'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Flag_of_Great_Britain_%281707%E2%80%931800%29.svg/1200px-Flag_of_Great_Britain_%281707%E2%80%931800%29.svg.png')),
-                        SizedBox(width: 20),
-                        Text("ENGLISH",style: TextStyle(color: Colors.white,fontSize: 15,fontWeight: FontWeight.bold)),
-                      ],
+              ),
+              child: Container(
+                width: 334,
+                height: 80,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      // width: 50,
+                      height: 80.0,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(30),
+                            topLeft: Radius.circular(30)),
+                        child: Image.network(
+                          'https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Flag_of_Vietnam.svg/1200px-Flag_of_Vietnam.svg.png',
+                          height: 80.0,
+                          width: 90.0,
+                          fit: BoxFit.fill,
+                        ),
+                      ),
                     ),
-                  ),
+                    SizedBox(width: 40),
+                    Text("VIETNAMESE",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 28,
+                            fontWeight: FontWeight.bold)),
+                  ],
                 ),
-              ],
+              ),
             ),
-          ),
-        ));
+            SizedBox(height: 20),
+            Card(
+              elevation: 5.0,
+              color: Color(0xff5CA4A7),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(30),
+                    topLeft: Radius.circular(30),
+                    bottomRight: Radius.circular(30),
+                    topRight: Radius.circular(30)),
+              ),
+              child: Container(
+                width: 334,
+                height: 80,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      // width: 100.0,
+                      height: 80.0,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(30),
+                            topLeft: Radius.circular(30)),
+                        child: Image.network(
+                          'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Flag_of_Great_Britain_%281707%E2%80%931800%29.svg/1200px-Flag_of_Great_Britain_%281707%E2%80%931800%29.svg.png',
+                          height: 80.0,
+                          width: 90.0,
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 40),
+                    Text("ENGLISH",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 28,
+                            fontWeight: FontWeight.bold)),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ));
   }
 }
