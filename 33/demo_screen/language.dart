feb59dc38f02fd23e257e3d8f6951f1168cea899
minhglhf <minhtq30102000@gmail.com>
2020-10-09 13:10:58 +0700

    tuong tac

--- 33/demo_screen/language.dart
+++ 33/demo_screen/language.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/level.dart';
 
 void main() {
   runApp(MyApp());
@@ -36,11 +37,10 @@ class _MyHomePageState extends State<MyHomePage> {
       backgroundColor: Color(0xff006265),
       body: Center(
         child: Column(
-
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
             SizedBox(height: 100),
-            Text("Select Your Language",
+            Text("SELECT YOUR LANGUAGE",
                 style: TextStyle(
                     color: Colors.white,
                     fontSize: 28,
@@ -50,42 +50,49 @@ class _MyHomePageState extends State<MyHomePage> {
               elevation: 5.0,
               color: Color(0xff5CA4A7),
               shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(30),
-                      topLeft: Radius.circular(30),
-                      bottomRight: Radius.circular(30),
-                      topRight: Radius.circular(30)
-                  ),
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(30),
+                    topLeft: Radius.circular(30),
+                    bottomRight: Radius.circular(30),
+                    topRight: Radius.circular(30)),
               ),
-              child: Container(
-                width: 334,
-                height: 80,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      // width: 50,
-                      height: 80.0,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.circular(30),
-                            topLeft: Radius.circular(30)),
-                        child: Image.network(
-                          'https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Flag_of_Vietnam.svg/1200px-Flag_of_Vietnam.svg.png',
-                          height: 80.0,
-                          width: 90.0,
-                          fit: BoxFit.fill,
+              child: new InkWell(
+                child: Container(
+                  width: 334,
+                  height: 80,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        // width: 50,
+                        height: 80.0,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(30),
+                              topLeft: Radius.circular(30)),
+                          child: Image.network(
+                            'https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Flag_of_Vietnam.svg/1200px-Flag_of_Vietnam.svg.png',
+                            height: 80.0,
+                            width: 90.0,
+                            fit: BoxFit.fill,
+                          ),
                         ),
                       ),
-                    ),
-                    SizedBox(width: 40),
-                    Text("VIETNAMESE",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 28,
-                            fontWeight: FontWeight.bold)),
-                  ],
+                      SizedBox(width: 40),
+                      Text("VIETNAMESE",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 28,
+                              fontWeight: FontWeight.bold)),
+                    ],
+                  ),
                 ),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => MyLevelPage()),
+                  );
+                },
               ),
             ),
             SizedBox(height: 20),
@@ -99,35 +106,43 @@ class _MyHomePageState extends State<MyHomePage> {
                     bottomRight: Radius.circular(30),
                     topRight: Radius.circular(30)),
               ),
-              child: Container(
-                width: 334,
-                height: 80,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    SizedBox(
-                      // width: 100.0,
-                      height: 80.0,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.circular(30),
-                            topLeft: Radius.circular(30)),
-                        child: Image.network(
-                          'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Flag_of_Great_Britain_%281707%E2%80%931800%29.svg/1200px-Flag_of_Great_Britain_%281707%E2%80%931800%29.svg.png',
-                          height: 80.0,
-                          width: 90.0,
-                          fit: BoxFit.fill,
+              child: new InkWell(
+                child: Container(
+                  width: 334,
+                  height: 80,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        // width: 100.0,
+                        height: 80.0,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(30),
+                              topLeft: Radius.circular(30)),
+                          child: Image.network(
+                            'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f2/Flag_of_Great_Britain_%281707%E2%80%931800%29.svg/1200px-Flag_of_Great_Britain_%281707%E2%80%931800%29.svg.png',
+                            height: 80.0,
+                            width: 90.0,
+                            fit: BoxFit.fill,
+                          ),
                         ),
                       ),
-                    ),
-                    SizedBox(width: 40),
-                    Text("ENGLISH",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 28,
-                            fontWeight: FontWeight.bold)),
-                  ],
+                      SizedBox(width: 40),
+                      Text("ENGLISH",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 28,
+                              fontWeight: FontWeight.bold)),
+                    ],
+                  ),
                 ),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => MyLevelPage()),
+                  );
+                },
               ),
             ),
           ],
