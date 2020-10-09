ae2e866bdfc8d4acf37caa1dd92209671f27dded
kev <hajuha168@gmail.com>
2020-10-09 13:28:48 +0700

    navigate

---
+++ 33/demo_screen/learn_kanji/lib/level-choose.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:learn_kanji/components/LevelCard.dart';
+
+class LevelChoose extends StatefulWidget {
+  LevelChoose({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _LevelChooseState createState() => _LevelChooseState();
+}
+
+class _LevelChooseState extends State<LevelChoose> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      backgroundColor: Color(0xff006465),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("SELECT YOUR LEVEL",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold)),
+            SizedBox(height: 150),
+            LevelCard("BEGINNER 1"),
+            SizedBox(height: 25),
+            LevelCard("BEGINNER 2"),
+            SizedBox(height: 25),
+            LevelCard("PRE-INTERMEDIATE 1"),
+            SizedBox(height: 25),
+            LevelCard("PRE-INTERMEDIATE 2"),
+          ],
+        ),
+      ),
+    ));
+  }
+}
