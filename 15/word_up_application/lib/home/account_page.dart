e937a8ae4631c02bcedf623f3c91a1bf30c57ab5
Trinh67 <56555813+Trinh67@users.noreply.github.com>
2020-10-15 00:03:10 +0700

    #20

--- 15/word_up_application/lib/home/account_page.dart
+++ 15/word_up_application/lib/home/account_page.dart
@@ -1,19 +1,92 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:word_up_application/size_config.dart';
 
-class AccountPage extends StatelessWidget{
+class AccountPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      color: Colors.transparent,
       child: Container(
-        width: SizeConfig.screenWidth / 3 * 2.2,
+        width: SizeConfig.screenWidth / 3 * 2.3,
         child: Drawer(
-          elevation: 300,
-          child: new ListView(
-            children: <Widget>[
-              new UserAccountsDrawerHeader(accountName: Text('Truong'), accountEmail: null)
-            ],
+          child: Container(
+            color: Colors.white,
+            child: new ListView(
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                new UserAccountsDrawerHeader(
+                  accountName: new Text(
+                    "Word Up",
+                    style: TextStyle(
+                        fontSize: 2.4 * SizeConfig.textMultiplier,
+                        color: Colors.white),
+                  ),
+                  accountEmail: new Text(
+                    "wordup15@gmail.com",
+                    style: TextStyle(
+                        fontSize: 1.8 * SizeConfig.textMultiplier,
+                        color: Colors.white),
+                  ),
+                  decoration: new BoxDecoration(
+                    image: new DecorationImage(
+                      image: new ExactAssetImage('assets/images/wordup.JPG'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  currentAccountPicture: CircleAvatar(
+                    backgroundColor: Colors.lightBlueAccent,
+                    child: Text(
+                      'W',
+                      style: TextStyle(
+                          fontSize: 4 * SizeConfig.textMultiplier,
+                          color: Colors.black),
+                    ),
+                  ),
+                ),
+                new ListTile(
+                    leading: Icon(Icons.person),
+                    title: new Text("My Account"),
+                    onTap: () {
+                      Navigator.pop(context);
+                    }),
+                new Divider(),
+                new ListTile(
+                    leading: Icon(Icons.info),
+                    title: new Text("About us"),
+                    onTap: () {
+                      Navigator.pop(context);
+                    }),
+                new ListTile(
+                    leading: Icon(Icons.feedback),
+                    title: new Text("Feedback"),
+                    onTap: () {
+                      Navigator.pop(context);
+                    }),
+                new ListTile(
+                    leading: Icon(Icons.help),
+                    title: new Text("Support"),
+                    onTap: () {
+                      Navigator.pop(context);
+                    }),
+                new Divider(),
+                new ListTile(
+                    leading: Icon(Icons.power_settings_new),
+                    title: new Text("Logout"),
+                    onTap: () {
+                      Navigator.pop(context);
+                    }),
+                new Container(
+                  margin: EdgeInsets.only(top: 3 * SizeConfig.heightMultiplier),
+                  child: Text(
+                    "WordUp",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        fontSize: 4.5 * SizeConfig.textMultiplier,
+                        color: Colors.lightBlueAccent),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
