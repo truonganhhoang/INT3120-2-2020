9589ba5ca1b1123c36363f962a74cc08c14450f7
Dung Nguyen <nguyenquocdung604@gmail.com>
2020-11-13 14:05:41 +0700

    Test 2

---
+++ 07/maths_game/test/test 2.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:maths_game/main.dart';
+import 'package:maths_game/SettingsScreen.dart';
+import 'package:maths_game/TrueOrFalseScreen.dart';
+import 'package:maths_game/TimeScreen.dart';
+import 'package:maths_game/GameModeScreen.dart';
+import 'package:maths_game/QuizScreen.dart';
+import 'package:maths_game/KienThucScreen.dart';
+import 'package:maths_game/ResultScreen.dart';
+
+void main() {
+  testWidgets('Settings test', (WidgetTester tester) async {
+    var screen = SettingsScreen();
+    BuildContext context;
+    screen.build(context);
+    var sound = MyMusicSlider();
+    var sound2 = sound.createState();
+    sound2.initState();
+    sound2.build(context);
+    var bright = MyBrigtnessSlider();
+    var bright2 = sound.createState();
+    bright2.initState();
+    bright2.build(context);
+  });
+  
+    testWidgets('KienThuc test', (WidgetTester tester) async {
+    BuildContext context;
+    var screen = KienThucScreen('plus');
+    expect(screen.calculation,'plus');
+    var screen2 = screen.createState();
+    screen2.Section(1);
+    screen2.build(context);
+    screen2.getData(2, 'plus');
+  });
+
+  testWidgets('GameMode test', (WidgetTester tester) async {
+    var screen = GameModeScreen('plus');
+    BuildContext context;
+    screen.build(context);
+    expect(screen.calculation,'plus');
+  });
+
+}
